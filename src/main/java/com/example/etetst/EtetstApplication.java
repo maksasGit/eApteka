f27ec28b187414354cab4e317b0e37f0eb05d197
maksasGit <maksim.rishko.vit@gmail.com>
2023-10-06 17:00:17 +0200

    Start
    олег подключи базу

---
+++ src/main/java/com/example/etetst/EtetstApplication.java
@@ -0,0 +1,13 @@
+package com.example.etetst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtetstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EtetstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/etetst/EtetstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.etetst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtetstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
