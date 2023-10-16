19eef827e7a6dce1591acc89abc8c5bb4ec9339a
maksasGit <maksim.rishko.vit@gmail.com>
2023-10-16 11:05:57 +0200

    add Product DB entity

---
+++ src/main/java/Entity/Product.java
@@ -0,0 +1,26 @@
+package Entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Float price;
+    //private Image image;
+    private Integer amount;
+}
