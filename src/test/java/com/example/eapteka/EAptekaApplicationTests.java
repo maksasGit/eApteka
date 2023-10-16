143c963019bea5997e77424ae02c032c5bc8396e
maksasGit <maksim.rishko.vit@gmail.com>
2023-10-16 10:46:03 +0200

    fix DB error

---
+++ src/test/java/com/example/eapteka/EAptekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eapteka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EAptekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
