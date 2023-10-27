60a448d8a539eadbd9b744957515e48f29686e25
oxolerd <olerdikon@gmail.com>
2023-10-27 16:24:24 +0200

    a

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch the list of users from your Spring Boot endpoint
+    axios.get('http://localhost:8080/users') // Replace with your Spring Boot server URL
+      .then(response => {
+        setUsers(response.data.content);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.username}
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
-export default App;
+export default UserList;
