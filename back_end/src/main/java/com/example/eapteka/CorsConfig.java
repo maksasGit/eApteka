45de9295b583d884b6ecaff54fea7f3495844063
oxolerd <olerdikon@gmail.com>
2023-11-01 22:37:18 +0100

    connected fron with /products endpoint

---
+++ back_end/src/main/java/com/example/eapteka/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.example.eapteka;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+
+        // Allow requests from a specific origin
+        config.addAllowedOrigin("http://localhost:3000");
+
+        // You can configure other CORS settings as needed, e.g., methods, headers, etc.
+        // config.addAllowedMethod("GET");
+        // config.addAllowedHeader("Content-Type");
+
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
---
+++ front_end/src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import api from './api/axiosConfig';
+import { useState, useEffect } from 'react';
+
+function App(){
+  const [products, setProducts] = useState();
+
+const getProducts = async () =>{
+  try
+  {
+    const response = await api.get("/products");
+    console.log(response.data);
+    setProducts(response.data);
+  }
+  catch(err){
+    console.log(err);
+  }
+}
+
+useEffect(() => {
+getProducts();
+}, []) 
+  return (
+    <div className="App">
+      
+    </div>
+  );
+
+  }
+
+export default App;
